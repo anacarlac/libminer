16b3625c40a9bbab49cfe5a544e79b0dad43d866
Ana Crispim <anacarlacrispim@gmail.com>
2023-09-18 11:05:08 -0500

    lib_summary

--- R/lib_summary.R
+++ R/lib_summary.R
@@ -2,7 +2,7 @@
 lib_summary <- function() {
   pkgs <- utils::installed.packages() # get the packages
   pkg_tbl <- table(pkgs[, "LibPath"]) # get the table with packages
-  pkg <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE) # turns the table in a dataframe object
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE) # turns the table in a dataframe object
   names(pkg_df) <- c("Library", "n_packages")
   pkg_df
 
